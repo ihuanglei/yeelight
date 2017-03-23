fcd3533d5c7cfc4035ad4543414c3963ea416c48
lei <leihuang@renrenbx.com>
2017-03-23 21:40:51 +0800

    modify

--- yeelight.py
+++ yeelight.py
@@ -5,7 +5,14 @@ import socket
 import sys
 import threading
 import logging
+import posixpath
+import urllib
+import urlparse
+import BaseHTTPServer
+import os
 
+
+__version__ = "0.1"
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
@@ -24,20 +31,12 @@ class Message:
 class YeeLight:
 
     @property
-    def host(self):
-        self._host
+    def location(self):
+        self._location
 
-    @host.setter
-    def host(self, value):
-        self._host = value
-
-    @property
-    def port(self):
-        self._port
-
-    @port.setter
-    def port(self, value):
-        self._port = value
+    @location.setter
+    def location(self, value):
+        self._location = value
 
     @property
     def id(self):
@@ -103,6 +102,13 @@ class YeeLight:
     def color_mode(self, value):
         self._color_mode = value
 
+    def to_dict(self):
+        dict = {}
+        for item in self.__dict__.items():
+            key, value = item
+            dict[key] = value
+        return dict
+
     def __str__(self):
         return '\n'.join(['%s:%s' % item for item in self.__dict__.items()])
 
@@ -111,7 +117,9 @@ class YeeLight:
 #
 
 
-class YeeLightApi:
+class YeeLightServer:
+
+    __instance = None
 
     SUDDEN = 'sudden'
 
@@ -127,16 +135,10 @@ class YeeLightApi:
     # ssdp notify
     NOTIFY = 'NOTIFY * HTTP/1.1'
 
-    # command custom
-    COMMAND = 'CMD * HTTP/1.1'
-
     # default yeelight addr
     HOST_YEELIGHT = '239.255.255.250'
 
     PORT_YEELIGHT = 1982
-
-    # default command addr
-    HOST_CMD = '239.255.255.251'
 
     # enter
     CR = '\r'
@@ -147,144 +149,158 @@ class YeeLightApi:
     # enter + new line
     CRLF = CR + LF
 
+    def __init__(self):
+        self._socket_scan = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._yeelights = []
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = super(Singleton, cls).__new__(
+                cls, *args, **kwargs)
+        return cls.__instance
+
     # yeelight command get_prop
-    @staticmethod
-    def get_prop(addr, arg):
-        return YeeLightApi._cmd(addr, 'get_prop', arg.get('prop', []))
+    def get_prop(self, addr, arg):
+        return self._cmd(addr, 'get_prop', arg.get('prop', []))
 
     # yeelight command set_scene
-    @staticmethod
-    def set_scene(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_scene', arg.get('class', []))
+    def set_scene(self, addr, arg):
+        return self._cmd(addr, 'set_scene', arg.get('class', []))
 
     # yeelight command power_on
-    @staticmethod
-    def power_on(addr, arg):
-        return YeeLightApi.set_power(addr, 'on', arg.get('effect', YeeLightApi.SMOOTH), arg.get('duration', 500))
+    def power_on(self, addr, arg):
+        return self.set_power(addr, 'on', arg.get('effect', self.SMOOTH), arg.get('duration', 500))
 
     # yeelight command power_off
-    @staticmethod
-    def power_off(addr, arg):
-        return YeeLightApi.set_power(addr, 'off', arg.get('effect', YeeLightApi.SMOOTH), arg.get('duration', 500))
+    def power_off(self, addr, arg):
+        return self.set_power(addr, 'off', arg.get('effect', self.SMOOTH), arg.get('duration', 500))
 
     # yeelight command set_power
-    @staticmethod
-    def set_power(addr, stat, effect, duration):
-        return YeeLightApi._cmd(addr, 'set_power', [stat, effect, duration])
+    def set_power(self, addr, stat, effect, duration):
+        return self._cmd(addr, 'set_power', [stat, effect, duration])
 
     # yeelight command start_cf
-    @staticmethod
-    def start_cf(addr, arg):
-        return YeeLightApi._cmd(addr, 'start_cf', [arg.get('count', 0), arg.get('action', 0), arg.get('flow_expression', "")])
+    def start_cf(self, addr, arg):
+        return self._cmd(addr, 'start_cf', [arg.get('count', 0), arg.get('action', 0), arg.get('flow_expression', "")])
 
     # yeelight command stop_cf
-    @staticmethod
-    def stop_cf(addr, arg):
-        return YeeLightApi._cmd(addr, 'stop_cf', [])
+    def stop_cf(self, addr, arg):
+        return self._cmd(addr, 'stop_cf', [])
 
     # yeelight command cron_add
-    @staticmethod
-    def cron_add(addr, arg):
-        return YeeLightApi._cmd(addr, 'cron_add', [0, arg.get('value', 0)])
+    def cron_add(self, addr, arg):
+        return self._cmd(addr, 'cron_add', [0, arg.get('value', 0)])
 
     # yeelight command cron_get
-    @staticmethod
-    def cron_get(addr, arg):
-        return YeeLightApi._cmd(addr, 'cron_get', [0])
+    def cron_get(self, addr, arg):
+        return self._cmd(addr, 'cron_get', [0])
 
     # yeelight command cron_del
-    @staticmethod
-    def cron_del(addr, arg):
-        return YeeLightApi._cmd(addr, 'cron_del', [0])
+    def cron_del(self, addr, arg):
+        return self._cmd(addr, 'cron_del', [0])
 
     # yeelight command set_adjust
-    @staticmethod
-    def set_adjust(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_adjust', [arg.get('action', 'increase'), arg.get('prop', 'bright')])
+    def set_adjust(self, addr, arg):
+        return self._cmd(addr, 'set_adjust', [arg.get('action', 'increase'), arg.get('prop', 'bright')])
 
     # yeelight command set_bright
-    @staticmethod
-    def set_bright(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_bright', [arg.get('brightness', 30),  arg.get('effect', YeeLightApi.SMOOTH), arg.get('duration', 500)])
+    def set_bright(self, addr, arg):
+        return self._cmd(addr, 'set_bright', [arg.get('brightness', 30),  arg.get('effect', self.SMOOTH), arg.get('duration', 500)])
 
     # yeelight command set_rgb
-    @staticmethod
-    def set_rgb(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_rgb', [arg.get('rgb', 16777215),  arg.get('effect', YeeLightApi.SMOOTH), arg.get('duration', 500)])
+    def set_rgb(self, addr, arg):
+        return self._cmd(addr, 'set_rgb', [arg.get('rgb', 16777215),  arg.get('effect', self.SMOOTH), arg.get('duration', 500)])
 
     # yeelight command set_hsv
-    @staticmethod
-    def set_hsv(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_hsv', [arg.get('hue', 0), arg.get('sat', 0), arg.get('effect', YeeLightApi.SMOOTH), arg.get('duration', 500)])
+    def set_hsv(self, addr, arg):
+        return self._cmd(addr, 'set_hsv', [arg.get('hue', 0), arg.get('sat', 0), arg.get('effect', self.SMOOTH), arg.get('duration', 500)])
 
     # yeelight command set_ct_abx
-    @staticmethod
-    def set_ct_abx(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_ct_abx', [arg.get('ct_value', 1700), arg.get('effect', YeeLightApi.SMOOTH), arg.get('duration', 500)])
+    def set_ct_abx(self, addr, arg):
+        return self._cmd(addr, 'set_ct_abx', [arg.get('ct_value', 1700), arg.get('effect', self.SMOOTH), arg.get('duration', 500)])
 
     # yeelight command set_default
-    @staticmethod
-    def set_default(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_default', [])
+    def set_default(self, addr, arg):
+        return self._cmd(addr, 'set_default', [])
 
     # yeelight command toggle
-    @staticmethod
-    def toggle(addr, arg):
-        return YeeLightApi._cmd(addr, 'toggle', [])
+    def toggle(self, addr, arg):
+        return self._cmd(addr, 'toggle', [])
 
     # yeelight command set_name
-    @staticmethod
-    def set_name(addr, arg):
-        return YeeLightApi._cmd(addr, 'set_name', arg.get('name', 'no name'))
+    def set_name(self, addr, arg):
+        return self._cmd(addr, 'set_name', arg.get('name', 'no name'))
 
     # yeelight command set_music
-    @staticmethod
-    def set_music(addr, arg):
+    def set_music(self, addr, arg):
         pass
 
+    # yeelight
+    def get_yeelights(self):
+        return self._yeelights
+
+    # yeelight command search
+    def search(self):
+        command = self.SEARCH + self.CRLF + \
+            'HOST: %s:%s' % (self.HOST_YEELIGHT, self.PORT_YEELIGHT) + self.CRLF + \
+            'MAN: "ssdp:discover"' + self.CRLF + \
+            'ST: wifi_bulb' + self.CRLF
+        self.socket_scan.sendto(
+            command, 0, (self.HOST_YEELIGHT, self.PORT_YEELIGHT))
+
+    # control command
+    def ccmd(self, location, method, param):
+        param = param or {}
+        try:
+            obj = json.loads(param)
+        except Exception, e:
+            obj = {}
+        try:
+            getattr(self, method)(location, obj)
+        except Exception, e:
+            logging.warning('method(%s) error', e)
+
     # parse header
-    @staticmethod
-    def _parse(data):
+    def _parse(self, data):
         message = Message()
-        headers = data.split(YeeLightApi.LF)
-        if headers and len(headers) > 0:
-            for header in headers:
-                header = header.strip()
-                if header == '':
-                    continue
-                kv = header.split(':', 1)
-                if kv and len(kv) > 1:
-                    key = kv[0].strip().lower()
-                    value = kv[1].strip()
-                else:
-                    key = 'status'
-                    value = kv[0]
-                setattr(message, key, value)
+        try:
+            headers = data.split(self.LF)
+            if headers and len(headers) > 0:
+                for header in headers:
+                    header = header.strip()
+                    if header == '':
+                        continue
+                    kv = header.split(':', 1)
+                    if kv and len(kv) > 1:
+                        key = kv[0].strip().lower()
+                        value = kv[1].strip()
+                    else:
+                        key = 'status'
+                        value = kv[0]
+                    setattr(message, key, value)
+        except Exception, e:
+            logging.error(e)
         return message
 
     # send command to yeelight
-    @staticmethod
-    def _cmd(addr, method, data):
+    def _cmd(self, addr, method, data):
         try:
             str = json.dumps(
                 {'id': int(time.time()), 'method': method, 'params': data})
-            logging.debug(str)
+            logging.debug('send command [%s]', str)
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(6)
-            strs = addr.split(':')
-            host = strs[0]
-            port = int(strs[1])
-            sock.connect((host, port))
-            sock.send(str + YeeLightApi.CRLF)
-            response = sock.recv(1024)
+            sock.settimeout(3)
+            host, port = addr.split(':')
+            sock.connect((host, int(port)))
+            sock.send(str + self.CRLF)
+            response = sock.recv(2048)
             sock.close()
             return response
         except Exception, e:
             logging.error(e)
 
     # discover
-    @staticmethod
-    def _discover(message, callback):
+    def _discover(self, message):
         try:
             yeelight = YeeLight()
             yeelight.id = message.id
@@ -295,102 +311,138 @@ class YeeLightApi:
             yeelight.hue = message.hue
             yeelight.rgb = message.rgb
             yeelight.bright = message.bright
-            addr = message.location.split('//')[1].split(':')
-            yeelight.host = addr[0]
-            yeelight.port = addr[1]
-            callback(yeelight)
+            yeelight.location = message.location
+
+            for tmp_yeelight in self._yeelights:
+                if yeelight.id == tmp_yeelight.id:
+                    self._yeelights.remove(tmp_yeelight)
+                    break
+
+            self._yeelights.append(yeelight)
+
         except Exception, e:
             logging.error('parse yeelight error(%s)', e)
 
-    # control command
-    @staticmethod
-    def _ccmd(command, *args):
-        try:
-            method = command.method
-            addr = command.location
-            obj = json.loads(command.param)
-            getattr(YeeLightApi, method)(addr, obj)
-        except ValueError, e:
-            logging.warning('params(%s) error', e)
-        except AttributeError, e:
-            logging.warning('method(%s) error', e)
-        except Exception, e:
-            logging.error(e)
-
     # empty
-    @staticmethod
-    def _empty(*args):
+    def _empty(self, *args):
         logging.debug('empty: %s', args)
 
-    # yeelight command search
-    @staticmethod
-    def _search(callback=None):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        command = YeeLightApi.SEARCH + YeeLightApi.CRLF + \
-            'HOST: %s:%s' % (YeeLightApi.HOST_YEELIGHT, YeeLightApi.PORT_YEELIGHT) + YeeLightApi.CRLF + \
-            'MAN: "ssdp:discover"' + YeeLightApi.CRLF + \
-            'ST: wifi_bulb' + YeeLightApi.CRLF
-        sock.sendto(command, 0, (YeeLightApi.HOST_YEELIGHT,
-                                 YeeLightApi.PORT_YEELIGHT))
-        data, addr = sock.recvfrom(1024)
-        sock.close()
-        logging.debug('Received:%s from %s',
-                      re.compile('[\r|\n]').sub(' ', data), addr)
-        message = YeeLightApi._parse(data)
-        yeelight = YeeLightApi._discover(message)
-        callback(yeelight)
-
-    # udp server
-    @staticmethod
-    def _start_server(callback):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        local_ip = socket.gethostbyname(socket.gethostname())
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # bind
-        sock.bind(('', YeeLightApi.PORT_YEELIGHT))
-        # add yeelight mcast
-        sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP,
-                        socket.inet_aton(YeeLightApi.HOST_YEELIGHT) + socket.inet_aton(local_ip))
-
-        # add cmd mcast
-        sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP,
-                        socket.inet_aton(YeeLightApi.HOST_CMD) + socket.inet_aton(local_ip))
-        c = {
-            YeeLightApi.COMMAND: YeeLightApi._ccmd,
-            YeeLightApi.NOTIFY: YeeLightApi._discover
-        }
+    # _start_scan server
+    def _start_scan(self):
         while True:
-            data, addr = sock.recvfrom(1024)
-            logging.debug('Received:%s from %s',
-                          re.compile('[\r|\n]').sub(' ', data), addr)
-            message = YeeLightApi._parse(data)
-            fun = c.get(message.status, YeeLightApi._empty)
-            fun(message, callback)
+            try:
+                data, addr = self._socket_scan.recvfrom(2048)
+                logging.debug('Received:%s from %s',
+                              re.compile('[\r|\n]').sub(' ', data), addr)
+                self._discover(self._parse(data))
+            except socket.error, e:
+                pass
+            time.sleep(2)
+        self._socket_scan.close()
+
+    # passive server
+    def _start_passive(self):
+        local_ip = socket.gethostbyname(socket.gethostname())
+        self._socket_passive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket_passive.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # bind
+        self._socket_passive.bind(('', self.PORT_YEELIGHT))
+        # add yeelight mcast
+        self._socket_passive.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP,
+                                        socket.inet_aton(self.HOST_YEELIGHT) + socket.inet_aton(local_ip))
+
+        while True:
+            try:
+                data, addr = self._socket_passive.recvfrom(2048)
+                logging.debug('Received:%s from %s',
+                              re.compile('[\r|\n]').sub(' ', data), addr)
+                self._discover(self._parse(data))
+            except socket.error, e:
+                logging.error(e)
 
         # leave yeelight mcast
-        sock.setsockopt(socket.SOL_IP, socket.IP_DROP_MEMBERSHIP,
-                        socket.inet_aton(YeeLightApi.HOST_YEELIGHT) +
-                        socket.inet_aton(local_ip))
-
-        # leave cmd mcast
-        sock.setsockopt(socket.SOL_IP, socket.IP_DROP_MEMBERSHIP,
-                        socket.inet_aton(YeeLightApi.HOST_CMD) +
-                        socket.inet_aton(local_ip))
-        sock.close()
+        self._socket_passive.setsockopt(socket.SOL_IP, socket.IP_DROP_MEMBERSHIP,
+                                        socket.inet_aton(HOST_YEELIGHT) +
+                                        socket.inet_aton(local_ip))
+        self._socket_passive.close()
 
     # start
-    @staticmethod
-    def start(callback=None):
-        # create thread
-        t = threading.Thread(
-            target=YeeLightApi._start_server, args=(callback,))
-        t.setDaemon(True)
-        t.start()
-        t.join()
+    def start(self):
+        # passive create  thread
+        t1 = threading.Thread(target=self._start_passive)
+        t1.setDaemon(True)
+        t1.start()
 
-    # search
-    def search(callback=None):
-        # create thread
-        t = threading.Thread(target=YeeLightApi._search, args=(callback,))
-        t.setDaemon(True)
-        t.start()
+        # scan create thread
+        t2 = threading.Thread(target=self._start_scan)
+        t2.setDaemon(True)
+        t2.start()
+
+
+class YeeLightHttpdServer(BaseHTTPServer.HTTPServer):
+
+    @property
+    def yeelight_server(self):
+        return self._yeelight_server
+
+    def run(self):
+        self._yeelight_server = YeeLightServer()
+        self._yeelight_server.start()
+        self.serve_forever()
+
+
+class YeeLightHttpdHandle(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def yeelight_handle(self, location, method, param=None):
+        return self.server.yeelight_server.ccmd(location, method, param)
+
+    def do_CMD(self):
+        method = self.headers.get('method', None)
+        location = self.headers.get('location', None)
+        param = self.headers.get('param', None)
+        self.send_response(200)
+        self.end_headers()
+        if method and location:
+            self.yeelight_handle(location, method, param)
+        return
+
+    def do_SEARCH(self):
+        self.send_response(200)
+        self.end_headers()
+        result = []
+        for yeelight in self.server.yeelight_server.get_yeelights():
+            result.append(yeelight.to_dict())
+        self.wfile.write(json.dumps(result))
+        return
+
+    def do_GET(self):
+        return
+
+    def translate_path(self, path):
+        # abandon query parameters
+        path = path.split('?', 1)[0]
+        path = path.split('#', 1)[0]
+        # Don't forget explicit trailing slash when normalizing. Issue17324
+        trailing_slash = path.rstrip().endswith('/')
+        path = posixpath.normpath(urllib.unquote(path))
+        words = path.split('/')
+        words = filter(None, words)
+        path = os.getcwd()
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir):
+                continue
+            path = os.path.join(path, word)
+        if trailing_slash:
+            path += '/'
+        return path
+
+
+def run():
+    httpd = YeeLightHttpdServer(("", 8866), YeeLightHttpdHandle)
+    httpd.run()
+
+if __name__ == '__main__':
+    run()
--- yeelight_client.py
+++ yeelight_client.py
@@ -1,22 +1,46 @@
 #-*- coding: utf-8 -*-
 
 import time
+import json
 from yeelight import *
 
-HOST_CMD = '239.255.255.251'
+HOST_CMD = '239.255.255.250'
 PORT_CMD = 1982
 
 
-def send(host, port, cmd, param):
-    command = 'CMD * HTTP/1.1\r\nlocation:%s:%s\r\nmethod:%s\r\nparam:%s' % (
-        host, port, cmd, param)
+def send():
+    s = '''
+NOTIFY * HTTP/1.1
+Host: 239.255.255.250:1982
+Cache-Control: max-age=3600
+Location: yeelight://192.168.1.239:55443
+NTS: ssdp:alive
+Server: POSIX, UPnP/1.0 YGLC/1
+id: 0x000000000015243f
+model: color
+fw_ver: 18
+support: get_prop set_default set_power toggle set_bright start_cf stop_cf set_scene
+cron_add cron_get cron_del set_ct_abx set_rgb
+power: on
+bright: 100
+color_mode: 2
+ct: 4000
+rgb: 16711680
+hue: 100
+sat: 35
+name: my_bulb
+'''
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(command, 0, (HOST_CMD, PORT_CMD))
-    sock.close()
+    print sock.sendto(s, 0, (HOST_CMD, PORT_CMD))
+    # data, addr = sock.recvfrom(2048)
+    # print data
+    # sock.close()
 
-host = '192.168.1.100'
-port = 1234
 
-send(host, port, 'power_on', '{}')
-time.sleep(5)
-send(host, port, 'power_off', '{}')
+# host = '192.168.1.100'
+# port = 1234
+
+# send(host, port, 'power_on', '{}')
+# time.sleep(5)
+# send(host, port, 'power_off', '{}')
+send()
